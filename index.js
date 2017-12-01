beb98bede060cda615c7d67c5f3921a907c33c7a
Okuno Zankoku <zankoku.okuno@gmail.com>
2017-12-01 08:46:27 -0500

    working alarm (but not user-configurable)

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ document.addEventListener("PinoutReady", function () {
         pinout.cycles.week.time = time
         pinout.cycles.year.time = time
         pinout.classic.time = time
+        if (time.getHours() === alarmTime.getHours()
+            && time.getMinutes() === alarmTime.getMinutes()) {
+            if (sound.paused) { // FIXME the alarm deserves a real state machine with a real design
+                sound.play()
+            }
+        }
     }
 
     pinout.here.draw_local_coords()
@@ -27,39 +33,11 @@ document.addEventListener("PinoutReady", function () {
         }
     })
 
+    var alarmTime = new Date()
+    alarmTime.setHours(7, 30, 0, 0)
+    pinout.alarm.add(alarmTime)
+
     update(new Date())
     window.setInterval(function () { update(new Date()) }, 1000)
 })
 
-
-
-
-
-function percentFromMidnight(time) {
-    time = moment(time)
-    // FIXME doesn't account for leap seconds
-    // TODO extend moment with secondOfDay
-    var secondsFromMidnight = 60*60*time.hour() + 60*time.minute() + time.second()
-    return secondsFromMidnight / (60*60*24)
-}
-
-function percentFromVernalEquinox(time) {
-    time = moment(time)
-    // FIXME doesn't account for leap years
-    // FIXME doesn't account for precession of the equinox
-    var vernal_equinox = moment(new Date(time.year(), 2, 20))
-    var daysFromVernalEquinox = time.dayOfYear() - vernal_equinox.dayOfYear()
-    if (daysFromVernalEquinox < 0) {
-        daysFromVernalEquinox += 365 // FIXME accounting from the wrong year's equinox
-    }
-    return daysFromVernalEquinox / 365
-}
-
-
-
-
-
-function offsetRadiusIntersectsCircle(delta, r, phi) {
-    var theta = phi + Math.asin(delta/r * Math.sin(phi))
-    return [Math.cos(theta), Math.sin(theta)]
-}
--- pinout.js
+++ pinout.js
@@ -1,6 +1,6 @@
 function thePinout() { return {
 
-now: (function (){
+now: (function (){ // DEBUG
     var self = {
         _elem: document.querySelector("#now"),
     }
@@ -39,24 +39,30 @@ here: (function(){
     }
     return self
 })(),
-// TODO humanized form
-classic: (function(){
+
+
+////// Alarms //////
+
+alarm: (function(){
     var self = {
-        _elem: document.querySelector("#classicText"),
-        set time(time) {
-            var year = "1" + time.getFullYear()
-            var month = time.getMonth() + 1
-            var day = time.getDate()
-            var hour = time.getHours()
-            var minute = time.getMinutes()
-            var second = time.getSeconds()
-            var timezone = time.getTimezoneOffset()
-            timezone = "UTC" + (timezone < 0 ? "+" : "-") + timezone / 60
-            self._elem.textContent = year+"-"+month+"-"+day+" "+hour+":"+minute+":"+second+" "+timezone
-        },
+        // _configElems: document.querySelector("#alarm-config"), // TODO
+        _drawElems: document.querySelector("#alarms"),
+        add: function (time) {
+            // TODO reusable make svg element
+            var svgns = "http://www.w3.org/2000/svg"
+            var shape = document.createElementNS(svgns, "path")
+            shape.setAttribute("d", "M0.004,0 L0,0.8 L-0.004,0 Z")
+            shape.setAttribute("transform", "rotate("+360*percentFromMidnight(time)+")")
+            shape.setAttribute("class", "hand")
+            self._drawElems.appendChild(shape)
+        }
     }
     return self
 })(),
+
+
+////// Visual Time Display //////
+
 cycles: {
     day: (function(){
         var self = {
@@ -96,5 +102,31 @@ cycles: {
         return self
     })(),
 },
+
+
+////// Linguistic Time Display //////
+
+// TODO humanized form
+classic: (function(){
+    var self = {
+        _elem: document.querySelector("#classicText"),
+        set time(time) {
+            var year = "1" + time.getFullYear()
+            var month = time.getMonth() + 1
+            var day = time.getDate()
+            var hour = time.getHours()
+            var minute = time.getMinutes()
+            var second = time.getSeconds()
+            var timezone = time.getTimezoneOffset()
+            timezone = "UTC" + (timezone < 0 ? "+" : "-") + timezone / 60
+            self._elem.textContent = year+"-"+month+"-"+day+" "+hour+":"+minute+":"+second+" "+timezone
+        },
+    }
+    return self
+})(),
+
+
+
+
 
 }}
